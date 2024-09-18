7fe578f11b25d3fed8c54e1b9855c2cac3c180a2
Elias Alejandro Diaz Cahuas <N00369765@upn.pe>
2024-09-18 16:53:11 -0500

    Se crean los nodos y las operaciones de las listas del proyecto.

---
+++ ListaDobleDesarme.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasoSoldex
+{
+    //Creación de las operaciones que ejecutará la lista "ListaDobleDesarme"
+    internal class ListaDobleDesarme
+    {
+    }
+}
